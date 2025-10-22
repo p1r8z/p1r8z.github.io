4390793e8ccbb4b2e49acc1dd253be5d9d010641
p1r8z <parrotz7497@gmail.com>
2025-10-22 17:50:24 -0400

    test

---
+++ app/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import HomePage from './Pages/HomePage/HomePage'
+
+function App() {
+
+  return (
+    <HomePage></HomePage>
+  )
+}
+
+export default App
---
+++ app/src/Pages/HomePage/HomePage.tsx
@@ -0,0 +1,10 @@
+import photo from '../../assets/DSC_2531 copy.jpg'
+
+
+const HomePage = () => {
+  return (
+    <img src={photo} alt="Home"   />
+  )
+}
+
+export default HomePage
